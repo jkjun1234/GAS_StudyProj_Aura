dac469f367cf1baf4fb838afbbedb10f744f8ea5
JunCo <jkjun1234@gmail.com>
2025-11-07 02:03:53 +0900

    Add : 캐릭터 베이스 무기 소켓 생성 및 무기 컴포넌트 생성

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -4,6 +4,10 @@
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
+	// 무기 생성
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
