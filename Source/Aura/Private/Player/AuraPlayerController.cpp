70e5cd43ccaad12ee6dfe22d200cec6ad84f80ab
JunCo <jkjun1234@gmail.com>
2025-11-12 02:28:07 +0900

    [feat] 플레이어 컨트롤러 클래스 생성 후 마우스 커서 및 IMC 설정하는 코드 추가

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,5 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// AuraPlayerController.cpp
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraInputContext);	// AuraInputContext 가 유효하지 않으면 실행 종료 및 에러 반환
+
+	// Subsystem은 싱글톤으로 게임 내 1개만 존재
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	// IMC(입력 매핑컨텍스트를 에디터에서 가져와 InputSubsystem으로 설정
+	Subsystem->AddMappingContext(AuraInputContext, 0);
+
+	bShowMouseCursor = true;	// TopDown 방식 게임을 위해 마우스 커서 표시
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// 마우스 커서 설정
+	FInputModeGameAndUI InputModeData;	// 입력모드를 제어하기 위한 데이터 
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);	// 뷰포트에 마우스가 갇히지 않도록 설정
+	InputModeData.SetHideCursorDuringCapture(false);	// 게임 플레이 시작시 커서가 사라지지않도록 설정
+	SetInputMode(InputModeData);	// 설정한 입력 모드를 적용
+}
 
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -1,10 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// AuraPlayerController.h
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
 
 /**
  * 
@@ -13,5 +15,12 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraInputContext; 
 };
