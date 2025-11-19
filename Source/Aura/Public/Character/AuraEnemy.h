e694264823241a410ec3dea0156973838cd971c4
JunCo <jkjun1234@gmail.com>
2025-11-20 01:52:00 +0900

    [feat] : EnemyInterface 생성 후 ActorHighLight 관련 가상함수 생성 [fix] : AuraEnemy.h에서 EnemyInterface 상속

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -1,17 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
-/**
- * 
- */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnhighlightActor() override;
 };
