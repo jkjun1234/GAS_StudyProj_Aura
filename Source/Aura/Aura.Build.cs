66b46acd61a0ae04410800b917cdecdb4a9c28ae
JunCo <jkjun1234@gmail.com>
2025-12-29 23:39:36 +0900

    [feature] GAS 사용을 위한 AttributeSet, AbilitySystemComponent 클래스 생성 후 Build.cs 파일에서 GamePlay 관련 모듈을 추가

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -8,7 +8,14 @@ public class Aura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core",
+			"CoreUObject",
+			"Engine", "InputCore",
+			"EnhancedInput",
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
