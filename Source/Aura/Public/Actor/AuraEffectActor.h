b97719d4ea1de3cd5ee79d7797ab34de2d292e81
JunCo <jkjun1234@gmail.com>
2026-03-10 00:00:10 +0900

    [feature] GE_PotionHeal으로 Duration타입인 Effect 클래스를 생성 해당 Effect를 사용하는 BP_CrystalHeal도생성 및 적용

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -20,7 +20,12 @@ protected:
 	// 블루프린트에서 호출할 효과 적용함수
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
-	
+
+	// 즉시 적용하는 효과를 담는 변수
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	// 주기동안 효과 적용하는 Effect를 담는 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 };
